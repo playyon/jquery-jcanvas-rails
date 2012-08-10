68207964a296bed8c94252e5123ceca9d003f677
RobAtHedtek <rob@hedtek.com>
2012-08-10 12:07:54 +0100

    Okay - this should work with Rails 3.2

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ jquery-jcanvas-rails
 
 Packages up the JCanvas JavaScript drawing library for the Rails asset pipeline.
 
-Rails: 3.2.8 and up
+Rails: 3.2 and up
 
 `jquery-jcanvas-rails` 5.5 includes `jquery.jcanvas` 5.5
 
--- lib/jquery/jcanvas/rails/engine.rb
+++ lib/jquery/jcanvas/rails/engine.rb
@@ -2,7 +2,7 @@ require 'rails'
 
 module Jquery
   module Jcanvas
-    if defined?(::Rails) and ::Rails.version >= "3.2.8"
+    if defined?(::Rails) and ::Rails.version >= "3.2"
       module Rails
         class Engine < ::Rails::Engine
         end
